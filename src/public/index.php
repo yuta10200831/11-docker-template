a5c5ddcb0f7a22845b110225e2fa5d76b627efb0
yuta10200831 <yfuse20ki@yahoo.co.jp>
2023-07-17 12:23:30 +0900

    課題①の提出

--- src/public/index.php
+++ src/public/index.php
@@ -1,2 +1,11 @@
 <?php
-echo 'Welcome TECH QUEST!';
+$arrayList = [
+	[3, 5, 1, 0, 9, 12],
+	[1, 2, 14, 10, 19, 1],
+	[0, 3, 3, 3, 7, 2]
+];
+
+foreach ($arrayList as $array) {
+  $max_value = max($array);
+  echo $max_value . "<br>";
+}
